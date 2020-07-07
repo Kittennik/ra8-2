3e20e5034ae688c34104612bd209b646e4b4fc4d
Kittennik <55207633+Kittennik@users.noreply.github.com>
2020-07-07 17:05:46 +0300

    Выполненное задание

---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useJsonFetch from '../useJsonFetch';
+
+export default function ErrorComponent(props) {
+  const { url } = props;
+  // eslint-disable-next-line no-unused-vars
+  const [data, loading, error] = useJsonFetch(url);
+
+  return (
+    <div>
+      <p className='title'>Ошибка</p>
+      <p className='status'>{error}</p>
+    </div>
+  );
+}
